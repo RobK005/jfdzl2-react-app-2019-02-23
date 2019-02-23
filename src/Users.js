18c8d409a7df1ed4a43a77da2cfc916131ab61b2
Robert Kwasnicki <rkwas005@wp.pl>
2019-02-23 10:44:08 +0100

    Added random users

---
+++ src/Users.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+
+class Users {
+
+    constructor(containerId) {
+      this.container = document.getElementById(containerId);
+
+      this.users = {};
+
+      this.init();
+    }
+
+    init() {
+      this.fetchUsers()
+    }
+
+    fetchUsers() {
+      fetch('https://randomuser.me/api/?results=5&gender=female')
+      .then(response => {
+        console.log('Hello mam odpowiedź');
+        console.log(response)
+        return response.json();
+      })
+      .then(data => {
+        this.users = data;
+        console.log(this.users);
+        this.render();
+      })
+      .catch(error => {
+        console.log('Mam błąd :-)')
+      })
+    }
+
+    render() {
+      const ul = document.createElement('ul');
+      if (!this.users.results) {
+        return;
+      }
+      this.users.results.forEach(elem => {
+        const li = document.createElement('li');
+        li.innerHTML = `${elem.name.first} ${elem.name.last}`;
+        ul.appendChild(li);
+      });
+      this.container.appendChild(ul);
+    }
+    // JSX
+    // <ul>
+    //   {this.users.results.map(elem) => (
+    //     <li>{elem.name} {ele.surname}</li>
+    //   )}
+    // </ul>
+  }
+
+  const list = new Users('root');
+
+export default Users
